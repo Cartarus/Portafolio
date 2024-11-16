ccbd9c7d38d75c2d454bdc8fcf972e170f761d22
Cristian Guerrero <cristian.guerrero@uptc.edu.co>
2024-11-16 12:06:44 -0500

    Se agregan enlaces de redes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,47 @@
 import logo from "../assets/CRLogo.png";
-import { FaLinkedin,FaGithub,FaInstagram } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 
 export const Navbar = () => {
   return (
-    <nav className='mb-20 flex items-center justify-between py-6 '>
-        <div className="flex flex-shrink-0 items-center">
-            <img src={logo} alt="logo"  className=" mx-2  w-10"/>
-        </div>
-        <div className="m-8 flex items-center justify-center gap-4  text-2xl ">
-            <FaLinkedin/>
-            <FaGithub/>
-            <FaInstagram/>
-            <FaSquareXTwitter/>
-        </div>
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img src={logo} alt="logo" className="mx-2 w-10" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a 
+          href="https://www.linkedin.com/in/cristian-camilo-guerrero-mendoza-462b76225/" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="hover:text-blue-400 transition-colors"
+        >
+          <FaLinkedin />
+        </a>
+        <a 
+          href="https://github.com/Cartarus" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="hover:text-gray-500 transition-colors"
+        >
+          <FaGithub />
+        </a>
+        <a 
+          href="https://www.instagram.com/cxrthxrxs/?theme=dark" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="hover:text-pink-600 transition-colors"
+        >
+          <FaInstagram />
+        </a>
+        <a 
+          href="https://twitter.com/cartarus28" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="hover:text-blue-500 transition-colors"
+        >
+          <FaSquareXTwitter />
+        </a>
+      </div>
     </nav>
-  )
-}
+  );
+};
